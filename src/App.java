193cfe86976e32028044dd3b86d5517bfb046f88
iDomas <domo.emailas@gmail.com>
2016-12-14 22:43:16 +0200

    added main class

---
+++ src/App.java
@@ -0,0 +1,14 @@
+/**
+ * Main class
+ */
+public class App {
+
+
+    // run method
+    public static void main(String[] args) {
+
+
+        // test
+        System.out.println("Hello GitHub!");
+    }
+}
