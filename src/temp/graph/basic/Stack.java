e785ece8372cbaeb23e33cb821ac96c7a162fcb7
iDomas <domo.emailas@gmail.com>
2016-12-14 23:26:58 +0200

    added basic graph code

---
+++ src/temp/graph/basic/Stack.java
@@ -0,0 +1,40 @@
+package graph.basic;
+
+/**
+ * Created by Pikis on 11/27/2016.
+ */
+public class Stack
+{
+    private final int SIZE = 20;
+    private int[] st;
+    private int top;
+
+    public Stack()
+    {
+        st = new int[SIZE];
+        top = -1;
+    }
+
+    public void push(int j)
+    {
+        st[++top] = j;
+    }
+
+    public int pop()
+    {
+        return st[top--];
+    }
+
+    public int peek()
+    {
+        return st[top];
+    }
+
+    public boolean isEmpty()
+    {
+        return (top == -1);
+    }
+
+
+
+}
---
+++ src/temp/graph/basic/Vertex.java
@@ -0,0 +1,18 @@
+package graph.basic;
+
+/**
+ * Created by Pikis on 11/25/2016.
+ */
+public class Vertex
+{
+    public char label;
+    public boolean wasVisited;
+
+
+    public Vertex(char lab)
+    {
+        label = lab;
+        wasVisited = false;
+    }
+
+}
