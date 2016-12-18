97c94235f43ecda3c63f341db7d071b7c608e429
Orestas <Theorestas@gmail.com>
2016-12-18 19:28:38 +0200

    Updated App.class, linked vertexes.

--- src/lt/kvk/i9/pavarde_vardas/App.java
+++ src/lt/kvk/i9/pavarde_vardas/App.java
@@ -12,29 +12,13 @@ public class App {
     public static void main(String[] args) {
         Graph graph = new Graph();
 
-        graph.addVertex("1A", "s1", "s2", "s3");
-        graph.addVertex("DFB", "s1", "s2", "s3");
-        graph.addVertex("C", "s1", "s2", "s3");
-
-        graph.addEdge(1, 0); // BA
-        graph.addEdge(0, 1); // BC
-        graph.addEdge(2, 0); // CA
-
+        graph.addEdge(0, 2); //DL
+        graph.addEdge(0, 3); //DG
+        graph.addEdge(0, 1); //DO
+        graph.addEdge(1, 3); //OG
+        graph.addEdge(2, 4); //LK
+        
         graph.dfs();
-
-        graph.printAllVertex();
-
-        graph.updateInputFile();
-
-        String str = "k";
-        String strToCompare = "5";
-        System.out.println((str.compareTo(strToCompare)));
-        if ((str.compareTo(strToCompare)) == -1) {
-            System.out.println(str + " daugiau uz " + strToCompare);
-        } else if ((str.compareTo(strToCompare)) == 1) {
-            System.out.println(str + " maziau uz " + strToCompare);
-        } else {
-            System.out.println("lygu");
-        }
+        
     }
 }
